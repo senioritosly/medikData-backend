d60e8619fed734feec7cb6223ee1232261d90750
Cristales <sirsly073@gmail.com>
2023-05-24 19:19:06 -0600

    Agendar Citas Routes

--- src/controllers/agendarcitas.controller.js
+++ src/controllers/agendarcitas.controller.js
@@ -4,10 +4,9 @@ const addCitas = {}
 
 addCitas.getClinicas = async (req, res) => {
     try {
-
-        const { data, error } = await supabase.
-            from('clinica')
-            .select('*');
+        const { data, error } = await supabase
+            .from('clinica')
+            .select('*')
 
         if (error) {
             console.log(error);
@@ -18,10 +17,11 @@ addCitas.getClinicas = async (req, res) => {
             return res.status(404).json({ error: 'Usuario no encontrado' });
         }
 
+        return res.json({ user: data });
     } catch (error) {
-        console.log(error)
+        console.log(error);
+        return res.status(500).json({ error: 'Error en el servidor' });
     }
-
-}
+};
 
 export default addCitas
--- src/routes/agendar-citas.routes.js
+++ src/routes/agendar-citas.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express"
-import addCitas from '../controllers/agendar-citas.controller.js'
+import addCitas from '../controllers/agendarcitas.controller.js'
 
 
 const router = Router()

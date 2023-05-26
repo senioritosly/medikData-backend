8ae7d9d82dacc7e04195ab23917d91d800b08942
Bryan España <bryanespana21550@gmail.com>
2023-05-26 15:45:06 -0600

    Ultimas modificaciones de resenias

--- src/controllers/resenias.controller.js
+++ src/controllers/resenias.controller.js
@@ -3,21 +3,33 @@ import supabase from "../database.js";
 const reseniasConect = {}
 reseniasConect.getResenias = async (req, res) => {
     try {
-        const { data, error } = await supabase.from('resenias').select('*')
+        let { data: resenias, error } = await supabase
+            .from('resenias')
+            .select(`
+            *,
+            medico:medico (nombres, apellidos, dpi),
+            paciente:autor (nombres, apellidos, dpi)
+        `);
 
-        if (error) {
-            console.log(error);
-            return res.status(500).json({ error: 'Error al obtener las resenias' });
+        if (error) throw error;
+
+        if (!resenias.length) {
+            return res.status(404).json({ error: 'No hay reseñas' });
         }
 
-        if (!data) {
-            return res.status(404).json({ error: 'No hay resenias' });
-        }
+        // transformar las reseñas para que incluyan los nombres en lugar de las IDs
+        resenias = resenias.map(resenia => ({
+            ...resenia,
+            medico: `${resenia.medico.nombres} ${resenia.medico.apellidos}`,
+            paciente: `${resenia.paciente.nombres} ${resenia.paciente.apellidos}`
+        }));
 
-        return res.json({ resenias: data });
+        return res.json({ resenias });
     } catch (error) {
         console.log(error);
         return res.status(500).json({ error: 'Error en el servidor' });
     }
 }
+
 export default reseniasConect;
+

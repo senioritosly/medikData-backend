fbcf46f23d2968fda975dc7fd91b1b39aa2db1fa
Cristales <mariocardona0501@gmail.com>
2023-11-16 18:12:59 -0600

    Update database.js

--- src/database.js
+++ src/database.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 const SUPABASE_URL = 'https://wjfuorfvxrtgbzrvwxan.supabase.co/'
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndqZnVvcmZ2eHJ0Z2J6cnZ3eGFuIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODM4NzkyMzgsImV4cCI6MTk5OTQ1NTIzOH0.GLTlM-7DE9XxfwXEevtK64-p65KLKeCy2dQC3NBOTy0'
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndqZnVvcmZ2eHJ0Z2J6cnZ3eGFuIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY4Mzg3OTIzOCwiZXhwIjoxOTk5NDU1MjM4fQ.IU6y2dg3ZxPD52ElQ8yv9jchWjaNgKqRaoFgQpI1mhU'
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
 

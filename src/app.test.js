2f76cd51e1964b8a6ffa437b9b252d0ccbe8b1f2
Javilejo <pra21486@uvg.edu.gt>
2023-11-17 19:29:51 -0600

    Auto stash before merge of "IC" and "origin/IC"

--- src/app.test.js
+++ src/app.test.js
@@ -16,7 +16,7 @@ describe('Tests for agendar-citas routes', () => {
     
     describe('GET /api/addcitas/especialidades/:id_clinica', () => {
         it('should return a 200 status and an object with an array of especialidades', async () => {
-            const id_clinica = 1; // Aquí debes poner un id_clinica válido para tu prueba.
+            const id_clinica = 1;
             const res = await request(app).get(`/api/addcitas/especialidades/${id_clinica}`);
 
             expect(res.statusCode).to.equal(200);
@@ -27,8 +27,8 @@ describe('Tests for agendar-citas routes', () => {
 
     describe('GET /api/addcitas/medico/:especialidad/:id_clinica', () => {
         it('should return a 200 status and an object with an array of medicos', async () => {
-            const id_clinica = 1; // Aquí debes poner un id_clinica válido para tu prueba.
-            const especialidad = 'Cardiología'; // Aquí debes poner una especialidad válida para tu prueba.
+            const id_clinica = 1; 
+            const especialidad = 'Cardiología'; 
             const res = await request(app).get(`/api/addcitas/medico/${especialidad}/${id_clinica}`);
 
             expect(res.statusCode).to.equal(200);
@@ -36,24 +36,18 @@ describe('Tests for agendar-citas routes', () => {
             expect(res.body.medico).to.be.an('array');
         });
     });
-
-    describe('GET /api/clinicasasociadas/:pacientetoken', () => {
-        it('should return a 200 status and an object with clinic information if patient has pending appointments', async () => {
-            const pacientetoken = '3021185000101'; // Utiliza el pacientetoken específico proporcionado
+    describe('Test de integración continua para getMedicosClinicas', () => {
+        it('Debería retornar los médicos de la misma clínica', async () => {
+            
+            const dpiEjemplo = '2244598600101'; 
+            const res = await request(app)
+                .get(`/api/medico/${dpiEjemplo}`); 
     
-            const res = await request(app).get(`/api/addcitas/clinicas-pendientes/${pacientetoken}`);
-    
+            
             expect(res.statusCode).to.equal(200);
             expect(res.body).to.be.an('object');
-            // Verifica la estructura o contenido de la respuesta según lo esperado
-            // Esperas recibir información de la clínica relacionada con citas pendientes del paciente.
-            expect(res.body.clinicas).to.be.an('array');
-            // Asegúrate de que la respuesta contenga la información de la clínica esperada
-            // (nombre, dirección, teléfono, etc.)
+            expect(res.body.medicos).to.be.an('array');
+            
         });
-    
-        // Puedes agregar más casos de prueba para manejar diferentes escenarios (sin citas pendientes, pacientetoken no existente, etc.)
     });
-    
-
-});
+})

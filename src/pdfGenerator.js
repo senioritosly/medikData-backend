feddc228e675f720d62cb2a1ca4ab2c537eeda39
Sly <mariocardona0501@gmail.com>
2023-11-17 18:59:36 -0600

    PDF Final

--- src/pdfGenerator.js
+++ src/pdfGenerator.js
@@ -19,15 +19,79 @@ function crearPDF(datos) {
 
     const docDefinition = {
         content: [
-            { text: 'Detalle de la Cita Médica', style: 'header' },
+            {   
+                columns: [
+                {
+                image: path.join(__dirname, './assets/logoMedicData.png'),
+                width: 100,
+                alignment: 'left',
+                margin: [0, 0, 0, 20] // Agrega un margen inferior al logo
+                },
+                {
+                    // Textos de Clínica y Dirección alineados a la derecha
+                    width: '*',
+                    text: [
+                        { text: `Clínica: ${datos.clinica.nombre}\n`, bold: true },
+                        { text: `Dirección: ${datos.clinica.direccion}`, bold: true }
+                    ],
+                    alignment: 'right',
+                    margin: [0, 0, 0, 20]
+                }
+                ]
+            },
+            { text: 'Informe Médico', style: 'header' },
             '\n',
-            { text: 'Datos de la Cita:', style: 'subheader' },
-            `ID de la Cita: ${datos.cita.citasid}`,
+            { 
+                table: {
+                    widths: ['*'], // Esto asegura que la tabla ocupe el 100% del ancho
+                    body: [
+                        [{ text: 'Datos del Médico:', style: 'subheader' }],
+                    ]
+                },
+                layout: {
+                    fillColor: function (rowIndex, node, columnIndex) {
+                        return (rowIndex === 0) ? '#06316B' : null; // Color de fondo para el encabezado
+                    }
+                }
+            },
+            `Nombre: ${datos.medico.full_name}`,
+            `Especialidad: ${datos.medico.especialidad}`,
+            `Número Colegiado: ${datos.medico.numerocolegiado}`,
+            `Teléfono: ${datos.medico.telefono}`,
+            `Correo Electrónico: ${datos.medico.correo}`,
+            '\n',
+            { 
+                table: {
+                    widths: ['*'], // Esto asegura que la tabla ocupe el 100% del ancho
+                    body: [
+                        [{ text: 'Datos de la Cita:', style: 'subheader' }],
+                        // ... Cuerpo de la sección de datos de la cita
+                    ]
+                },
+                layout: {
+                    fillColor: function (rowIndex, node, columnIndex) {
+                        return (rowIndex === 0) ? '#06316B' : null; // Color de fondo para el encabezado
+                    }
+                }
+             },
             `Fecha: ${datos.cita.fecha}`,
             `Hora: ${datos.cita.hora}`,
             `Estado: ${datos.cita.estado}`,
             '\n',
-            { text: 'Datos del Paciente:', style: 'subheader' },
+            {
+                table: {
+                    widths: ['*'], // Esto asegura que la tabla ocupe el 100% del ancho
+                    body: [
+                        [{ text: 'Datos del Paciente:', style: 'subheader' }],
+                        // ... Cuerpo de la sección de datos de la cita
+                    ]
+                },
+                layout: {
+                    fillColor: function (rowIndex, node, columnIndex) {
+                        return (rowIndex === 0) ? '#06316B' : null; // Color de fondo para el encabezado
+                    }
+                }
+            },
             `Nombre: ${datos.paciente.full_name}`,
             `DPI: ${datos.paciente.dpi}`,
             `Altura: ${datos.paciente.altura} cm`,
@@ -37,19 +101,36 @@ function crearPDF(datos) {
             `Sexo: ${datos.paciente.sexo}`,
             `Correo Electrónico: ${datos.paciente.correo}`,
             '\n',
-            { text: 'Datos del Médico:', style: 'subheader' },
-            `Nombre: ${datos.medico.full_name}`,
-            `Especialidad: ${datos.medico.especialidad}`,
-            `Número Colegiado: ${datos.medico.numerocolegiado}`,
-            `Teléfono: ${datos.medico.telefono}`,
-            `Correo Electrónico: ${datos.medico.correo}`,
-            '\n',
-            { text: 'Datos de la Clínica:', style: 'subheader' },
+            { 
+                table: {
+                    widths: ['*'], // Esto asegura que la tabla ocupe el 100% del ancho
+                    body: [
+                        [{ text: 'Datos de la Clínica:', style: 'subheader' }],
+                    ]
+                },
+                layout: {
+                    fillColor: function (rowIndex, node, columnIndex) {
+                        return (rowIndex === 0) ? '#06316B' : null; // Color de fondo para el encabezado
+                    }
+                }
+            },
             `Nombre: ${datos.clinica.nombre}`,
             `Dirección: ${datos.clinica.direccion}`,
             `Teléfono: ${datos.clinica.telefono}`,
             '\n',
-            { text: 'Diagnóstico:', style: 'subheader' },
+            { 
+                table: {
+                    widths: ['*'], // Esto asegura que la tabla ocupe el 100% del ancho
+                    body: [
+                        [{ text: 'Diagnóstico:', style: 'subheader' }],
+                    ]
+                },
+                layout: {
+                    fillColor: function (rowIndex, node, columnIndex) {
+                        return (rowIndex === 0) ? '#06316B' : null; // Color de fondo para el encabezado
+                    }
+                }
+                 },
             `${datos.diagnostico.diagnostico}`,
         ],
         styles: {
@@ -62,7 +143,7 @@ function crearPDF(datos) {
             subheader: {
                 fontSize: 15,
                 bold: true,
-                color: '#06316B'
+                color: '#ffffff',
             }
         }
     };
